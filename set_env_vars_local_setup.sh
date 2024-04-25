8c76c078a72edf04b40f1836b472adb5cb00b3ea
Marius Goetze <marius.goetze@greenbone.net>
2024-04-25 17:23:38 +0200

    add: add config (configurable via env vars)

---
+++ set_env_vars_local_setup.sh
@@ -0,0 +1,7 @@
+# volues for which we don't have a default
+export DB_USERNAME=postgres
+export DB_PASSWORD=password
+export DB_SSL_MODE=disable
+
+# log level for convenience
+export LOG_LEVEL=debug
